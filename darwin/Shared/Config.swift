b69695e107700fc498699df119f0e251ac2c97b8
Rody Davis <rody.davis.jr@gmail.com>
2021-06-17 15:53:30 -0700

    switching ios/macos top multiplatform

---
+++ darwin/Shared/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  app
+//
+//  Created by Rody Davis on 6/17/21.
+//
+
+import Foundation
---
+++ darwin/Shared/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  app
+//
+//  Created by Rody Davis on 6/17/21.
+//
+
+import Foundation
