df2a116746b5086e0e20ad73a21833117609124a
Rody Davis <rody.davis.jr@gmail.com>
2021-06-16 18:52:45 -0700

    adding macos

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Rody Davis on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
