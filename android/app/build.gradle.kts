bff65d4457abe0fe38b75cb2ff6ff7b6b1bfb6a8
Rody Davis <rody.davis.jr@gmail.com>
2021-06-22 01:01:41 -0700

    script wip

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,12 +39,10 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
-        kotlinCompilerVersion = "1.4.32"
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
@@ -58,3 +56,14 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 }
+
+allprojects {
+    afterEvaluate {
+        tasks.preBuild {
+            val file = File("../build")
+            val target = File("$projectDir/app/src/main/res/build")
+            file.copyRecursively(target, overwrite = true)
+        }
+    }
+}
+
